db721ddd5e9fb10dadd87de02b8c251d21a6dda3
Franziska Becker <franziska.becker@vis.uni-stuttgart.de>
2022-07-14 21:18:44 +0200

    Adde resize handler in renderer

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -2,13 +2,29 @@ import * as THREE from 'three';
 
 export default class Renderer {
 
-    constructor() {}
+    constructor() {
+        this.readWindowDimension();
+        window.addEventListener('resize', () => {
+            // clear the timeout
+            clearTimeout(this.resizeTimeout);
+            // start timing for event "completion"
+            this.resizeTimeout = setTimeout(this.readWindowDimension.bind(this), 75);
+          });
+    }
+
+    readWindowDimension() {
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+        if (this.renderer) {
+            this.renderer.setSize(this.width, this.height);
+        }
+    }
 
     setupRenderer() {
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
         this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setSize(this.width, this.height);
         document.body.appendChild(this.renderer.domElement);
 
         this.camera.position.z = 5;
