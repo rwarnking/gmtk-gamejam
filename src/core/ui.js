85e0ed20c2e72b0b90a1f0ba92e3216f2b5560ea
Franziska Becker <franziska.becker@vis.uni-stuttgart.de>
2022-07-17 19:50:39 +0200

    Added intro fairy

--- src/core/ui.js
+++ src/core/ui.js
@@ -1,3 +1,5 @@
+import * as THREE from "three"
+import Component from "../logic/component";
 import TextureCycle from "../logic/prefabs/texture-cycle";
 import UIElement from "../logic/prefabs/ui-element";
 import GAME from "./globals";
@@ -5,11 +7,13 @@ import GAME from "./globals";
 const UI_POSITIONS = [
     // [-2.0, 3.0],
     // [-1.0, 3.0],
-    [+2.5, 3.0],
+    [-3.0, -2.0],
+    [2.5, 3.0],
 ];
 const UI_TEXTURES = [
     // "assets/sprites/dice-preview.png",
     // "assets/sprites/dice-goal.png",
+    "assets/sprites/fairy2story.png",
     "assets/sprites/restart-button_up.png",
 ];
 
@@ -28,7 +32,7 @@ export default class UI {
                 UI_TEXTURES[i]
             ));
         })
-        const last = this.elements[this.elements.length-1];
+        const last = this.elements[1];
         last.getPicking()
             .setClickFunc(GAME.restartLevel);
         last.addComponent(TextureCycle.createToggle(
@@ -36,6 +40,27 @@ export default class UI {
             ["assets/sprites/restart-button_up.png", "assets/sprites/restart-button_down.png"],
             150
         ));
+
+        const fairy = this.elements[0];
+        fairy.getObject3D().scale.set(6, 4, 1);
+        const maxLife = 40000, threshold = 1000;
+        fairy.life = maxLife;
+        fairy.one = false;
+        const killComp = new Component(fairy, function(_, delta) {
+            fairy.life -= delta;
+            if (!fairy.once && fairy.life <= maxLife * 0.5) {
+                fairy.once = true;
+                fairy.getObject3D().material.map = new THREE.TextureLoader().load("assets/sprites/fairy2wasd.png")
+            } else if (fairy.life < threshold) {
+                const frac = fairy.life / threshold;
+                fairy.getObject3D().material.opacity = frac;
+                if (frac <= 0) {
+                    fairy.getObject3D().visible = false;
+                }
+            }
+        })
+        fairy.addComponent(killComp);
+
     }
 
     forEach(callback) {
